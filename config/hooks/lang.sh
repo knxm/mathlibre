be87d203e55b7eb119e46873a6721e95db6213a2
Tatsuyoshi HAMADA <hamada@holst.sm.fukuoka-u.ac.jp>
2013-02-22 10:56:10 +0900

    Adding config file for lxpanel

--- config/hooks/lang.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# script for language settings 
-sed -i -e 's/kseg_en/kseg_ja/g' /etc/skel/.kseg
-sed -i -e 's/kseg_en/kseg_ja/g' /etc/skel/.kseg
-sed -i -e 's/kseg_en/kseg_ja/g' /etc/skel/.kseg
-sed -i -e 's/kseg_en/kseg_ja/g' /etc/skel/.kseg
-sed -i -e 's/kseg_en/kseg_ja/g' /etc/skel/.kseg
