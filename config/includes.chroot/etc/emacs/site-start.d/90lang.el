ef7b7456e6df16b953c4b6f7092ba1fbf7c97049
Debian Live user <user@debian.(none)>
2013-04-02 21:56:42 +0900

    Language setting file for multilingual environment

---
+++ config/includes.chroot/etc/emacs/site-start.d/90lang.el
@@ -0,0 +1,16 @@
+;;
+;; /etc/emacs/site-start.d/90lang.el
+;;    mathlibre language settings (input methods etc.)
+;;
+(let ((lang (getenv "LANG")))
+  (cond ((or (equal lang "ja_JP.UTF-8")
+         (equal lang "ja_JP.EUC-JP"))
+     (when (file-readable-p "/usr/share/emacs/site-lisp/emacs-mozc/mozc.el")
+       (require 'mozc)
+       (setq default-input-method "japanese-mozc"))
+     (when (file-readable-p "/usr/share/emacs/site-lisp/yatex/yatex.el")
+       (setq auto-mode-alist
+         (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+         (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t))
+     )
+    (t nil)))
