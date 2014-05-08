e4b4fad52528063736679f6318227af9b95a33a0
Tatsuyoshi HAMADA <hamada@holst.sm.fukuoka-u.ac.jp>
2014-05-08 18:42:37 +0900

    Adding settings for Latexmk with YaTeX

--- config/includes.chroot/etc/emacs/site-start.d/90lang.el
+++ config/includes.chroot/etc/emacs/site-start.d/90lang.el
@@ -13,5 +13,6 @@
        (setq auto-mode-alist
          (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
          (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t))
+     	 (setq tex-command "latexmk -pvc")
      )
     (t nil)))
